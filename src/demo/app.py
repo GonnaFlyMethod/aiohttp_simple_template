1191c42cc4fa7616c8e9e5cfa25b3a36161cea5a
GonnaFlyMethod <alexandergusakov@yahoo.com>
2021-08-30 20:27:27 +0300

    Add asyncpg support

--- src/demo/app.py
+++ src/demo/app.py
@@ -1,12 +1,30 @@
-from aiohttp import web
-from .routes import setup_routes
-
 import jinja2
 import aiohttp_jinja2
+import urllib.parse as up
+import asyncpg
+
+from aiohttp import web
+
+from .routes import setup_routes
+from .config import config_obj as config
+
+
+
+async def on_start(app):
+	up.uses_netloc.append("postgres")
+	url = up.urlparse(config['database_url'])
+	connection = await asyncpg.connect(user=url.username,
+									   password=url.password,
+                                       database=url.path[1:],
+                                       host=url.hostname,
+                                       port=url.port)
+	config['db'] = connection
+	print("Connected to db!")
 
 
 async def create_app():
 	app = web.Application()
 	aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('demo', 'templates'))
 	setup_routes(app)
-	return app	
+	await on_start(app)
+	return app
---
+++ src/demo/config.py
@@ -0,0 +1,7 @@
+from collections import defaultdict
+
+config_obj = defaultdict(lambda: 0)
+config_obj['db'] = None
+
+
+config_obj['database_url'] = 'postgresql://demo:demo@localhost:5432/demo'
